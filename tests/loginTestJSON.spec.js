43b2f32c0aa7163b2b2d40d575bbfb1ab47ef4e7
unknown <hareeshm.qa@gmail.com>
2026-06-25 12:27:11 +0530

    second commit

--- tests/loginTestJSON.spec.js
+++ tests/loginTestJSON.spec.js
@@ -6,7 +6,7 @@ test.only('login user with valid credentials using JSON', async ({page}) => {
     const loginPage = new LoginPage(page)
 
     await loginPage.goTo("/")
-    //console.log(process.env.USER_NAME,process.env.PASSWORD,process.env.NAMEOFTHEUSER)
+    
     await loginPage.enterCredntails(testdata.username,testdata.password,testdata.nameofuser)
 
 })
